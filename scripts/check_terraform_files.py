eb8ec2ef200edf367fa271bb3e7f3269a510f4b8
Douglas Mendes <douglasleomendes@hotmail.com>
2024-07-15 12:00:07 +0100

    fix the output

--- scripts/check_terraform_files.py
+++ scripts/check_terraform_files.py
@@ -12,11 +12,13 @@ if __name__ == "__main__":
     terraform_files = has_terraform_files()
     if terraform_files:
         print(f"Terraform files found: {terraform_files}")
-        # Define a saída
-        print("::set-output name=terraform_files_found::true")
+        # Define a saída usando arquivos de ambiente
+        with open(os.environ['GITHUB_ENV'], 'a') as env_file:
+            env_file.write("TERRAFORM_FILES_FOUND=true\n")
         exit(0)
     else:
         print("No Terraform files found.")
-        # Define a saída
-        print("::set-output name=terraform_files_found::false")
+        # Define a saída usando arquivos de ambiente
+        with open(os.environ['GITHUB_ENV'], 'a') as env_file:
+            env_file.write("TERRAFORM_FILES_FOUND=false\n")
         exit(1)
