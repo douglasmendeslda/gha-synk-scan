792b71651f90103b154f7cf1bf1a7f3e7dd3d2d0
Douglas Mendes <douglasleomendes@hotmail.com>
2024-07-15 11:56:49 +0100

    fix script

--- scripts/check_terraform_files.py
+++ scripts/check_terraform_files.py
@@ -1,15 +1,17 @@
 import os
 
 def has_terraform_files():
+    terraform_files = []
     for root, dirs, files in os.walk("."):
         for file in files:
             if file.endswith(".tf"):
-                return True
-    return False
+                terraform_files.append(os.path.join(root, file))
+    return terraform_files
 
 if __name__ == "__main__":
-    if has_terraform_files():
-        print("Terraform files found.")
+    terraform_files = has_terraform_files()
+    if terraform_files:
+        print(f"Terraform files found: {terraform_files}")
         exit(0)
     else:
         print("No Terraform files found.")
