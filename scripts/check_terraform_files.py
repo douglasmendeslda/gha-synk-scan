83e68b537cde71a23d3c085d207d1a2950720713
Douglas Mendes <douglasleomendes@hotmail.com>
2024-07-15 11:58:37 +0100

    add github output

--- scripts/check_terraform_files.py
+++ scripts/check_terraform_files.py
@@ -12,7 +12,11 @@ if __name__ == "__main__":
     terraform_files = has_terraform_files()
     if terraform_files:
         print(f"Terraform files found: {terraform_files}")
+        # Define a saída
+        print("::set-output name=terraform_files_found::true")
         exit(0)
     else:
         print("No Terraform files found.")
+        # Define a saída
+        print("::set-output name=terraform_files_found::false")
         exit(1)
