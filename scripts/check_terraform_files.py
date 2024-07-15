c5b70cf14da4e807a2998af4894761531aeb0e41
Douglas Mendes <douglasleomendes@hotmail.com>
2024-07-15 14:47:55 +0100

    refactor action

--- scripts/check_terraform_files.py
+++ scripts/check_terraform_files.py
@@ -12,12 +12,10 @@ if __name__ == "__main__":
     terraform_files = has_terraform_files()
     if terraform_files:
         print(f"Terraform files found: {terraform_files}")
-        # Define a saída usando arquivos de ambiente
         with open(os.environ['GITHUB_ENV'], 'a') as env_file:
             env_file.write("TERRAFORM_FILES_FOUND=true\n")
     else:
         print("No Terraform files found.")
-        # Define a saída usando arquivos de ambiente
         with open(os.environ['GITHUB_ENV'], 'a') as env_file:
             env_file.write("TERRAFORM_FILES_FOUND=false\n")
     exit(0)
